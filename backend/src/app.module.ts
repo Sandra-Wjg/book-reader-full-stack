fcfa0995439ff68a0a7d09f3c51b847ae439b609
Sandra <19938170528@163.com>
2025-12-17 14:55:38 +0800

    初步创建类型及数据库连接

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './modules/user/user.controller';
+import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'wjg1028',
+      database: 'vben',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
